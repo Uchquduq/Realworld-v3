3be64249e1bad81244baf6551b4337e856765f16
Rufat <rufatxasanov1998@gmail.com>
2022-08-28 02:40:58 +0500

    EventDetails

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 
 const routes = [{
         path: '/',
         name: 'EventList',
         component: EventList
+    },
+    {
+        path: '/event/:id',
+        name: 'EventDetails',
+        props: true,
+        component: EventDetails
     },
     {
         path: '/about',
