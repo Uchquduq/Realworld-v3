aaab1fb7658e9beb4474387d19afef9d08ceb4b5
Rufat <rufatxasanov1998@gmail.com>
2022-08-29 14:56:43 +0500

    Flash Messages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <nav>
+    <div class="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
     <router-link
       class="mb-2 text-1xl font-bold tracking-tight text-gray-900"
       :to="{ name: 'EventList' }"
@@ -15,7 +18,31 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+  inject: ["GStore"],
+};
+</script>
+
 <style lang="scss">
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+.flashMessage {
+
+  margin: 10px;
+  padding: 5px;
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,7 +53,7 @@
 
 nav {
   padding: 30px;
-
+  
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -8,13 +8,21 @@
 <script>
 export default {
   name: "EventDetails",
-  props: ['event'],
+  props: ["event"],
+  inject: ["GStore"],
   methods: {
     register() {
+      this.GStore.flashMessage =
+        "You are succesfully registered for " + this.event.title;
+
+      setTimeout(() => {
+        this.GStore.flashMessage = "";
+      }, 3000);
+
       this.$router.push({
-        name: 'EventDetails'
-      })
-    }
-  }
+        name: "EventDetails",
+      });
+    },
+  },
 };
 </script>
